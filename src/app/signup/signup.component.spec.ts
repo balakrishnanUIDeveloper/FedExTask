27b4a65b88425b40beeeef4ce82e7d75f80a3f56
balakrishnanUIDeveloper <krish.18.2009@gmail.com>
2022-08-16 19:07:06 +0100

    doc: documentation changes (#2)
    * docs: documentation changes
    
    * refactor: test case fix

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -30,20 +30,125 @@ describe('SignupComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  afterEach(() => {
+    fixture.destroy();
+  });
   it('should create', () => {
     expect(component).toBeDefined();
   });
   it('can load instance', () => {
     expect(component).toBeTruthy();
   });
-  it('Set firstname text value', async () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const firstName = fixture.debugElement.query(By.css('#firstName'));
-      firstName.nativeElement.value = 'john';
-      firstName.nativeElement.dispatchEvent(new Event('input'));
+  describe('Input validation check', () => {
+    it('firstname should throw error when input value contains non alpahbetical characters', async () => {
       fixture.detectChanges();
-      expect(firstName.nativeElement.value).toContain('john');
+      const firstName = fixture.nativeElement.querySelector('#firstName');
+      firstName.value = 'john123';
+      firstName.dispatchEvent(new InputEvent('focus'));
+      firstName.dispatchEvent(new InputEvent('blur'));
+      fixture.detectChanges();
+      expect(firstName.value).toEqual('john123');
+      fixture.whenStable().then(() => {
+        const firstName = fixture.nativeElement.querySelector('#fn_error');
+        expect(firstName.innerHTML).toContain(
+          component.fields_label.first_name.error
+        );
+      });
+    });
+    it('firstname should not throw error when input value contains alpahbetical characters', async () => {
+      fixture.detectChanges();
+      const firstName = fixture.nativeElement.querySelector('#firstName');
+      firstName.value = 'john';
+      firstName.dispatchEvent(new InputEvent('focus'));
+      firstName.dispatchEvent(new InputEvent('change'));
+      fixture.detectChanges();
+      expect(firstName.value).toEqual('john');
+      fixture.whenStable().then(() => {
+        const firstName = fixture.nativeElement.querySelector('#fn_error');
+        expect(firstName).toBeNull();
+      });
+    });
+    it('lastname should throw error when input value contains non alpahbetical characters', async () => {
+      fixture.detectChanges();
+      const lastname = fixture.nativeElement.querySelector('#lastName');
+      lastname.value = 'john123';
+      lastname.dispatchEvent(new InputEvent('focus'));
+      lastname.dispatchEvent(new InputEvent('blur'));
+      fixture.detectChanges();
+      expect(lastname.value).toEqual('john123');
+      fixture.whenStable().then(() => {
+        const lastname = fixture.nativeElement.querySelector('#ln_error');
+        expect(lastname.innerHTML).toContain(
+          component.fields_label.last_name.error
+        );
+      });
+    });
+    it('lastname should not throw error when input value contains alpahbetical characters', async () => {
+      fixture.detectChanges();
+      const lastname = fixture.nativeElement.querySelector('#lastName');
+      lastname.value = 'john';
+      lastname.dispatchEvent(new InputEvent('focus'));
+      lastname.dispatchEvent(new InputEvent('change'));
+      fixture.detectChanges();
+      expect(lastname.value).toEqual('john');
+      fixture.whenStable().then(() => {
+        const lastname = fixture.nativeElement.querySelector('#ln_error');
+        expect(lastname).toBeNull();
+      });
+    });
+    it('email should throw error when input value contains non alpahbetical characters', async () => {
+      fixture.detectChanges();
+      const email = fixture.nativeElement.querySelector('#email');
+      email.value = 'test@gmail';
+      email.dispatchEvent(new InputEvent('focus'));
+      email.dispatchEvent(new InputEvent('blur'));
+      fixture.detectChanges();
+      expect(email.value).toEqual('test@gmail');
+      fixture.whenStable().then(() => {
+        const email = fixture.nativeElement.querySelector('#email_error');
+        expect(email.innerHTML).toContain(component.fields_label.email.error);
+      });
+    });
+    it('email should not throw error when input value contains non alpahbetical characters', async () => {
+      fixture.detectChanges();
+      const email = fixture.nativeElement.querySelector('#email');
+      email.value = 'test@gmail.com';
+      email.dispatchEvent(new InputEvent('focus'));
+      email.dispatchEvent(new InputEvent('change'));
+      fixture.detectChanges();
+      expect(email.value).toEqual('test@gmail.com');
+      fixture.whenStable().then(() => {
+        const email = fixture.nativeElement.querySelector('#email_error');
+        expect(email).toBeNull();
+      });
+    });
+    it('password should throw error when input value contains invalid characters', async () => {
+      fixture.detectChanges();
+      const password = fixture.nativeElement.querySelector('#password');
+      password.value = 'test@';
+      password.dispatchEvent(new InputEvent('focus'));
+      password.dispatchEvent(new InputEvent('blur'));
+      fixture.detectChanges();
+      expect(password.value).toEqual('test@');
+      fixture.whenStable().then(() => {
+        const password = fixture.nativeElement.querySelector('#password_error');
+        expect(password.innerHTML).toContain(
+          component.fields_label.password.error
+        );
+      });
+    });
+    it('password should not throw error when input value contains valid characters', async () => {
+      fixture.detectChanges();
+      const password = fixture.nativeElement.querySelector('#password');
+      password.value = 'abcdefghij';
+      password.dispatchEvent(new InputEvent('focus'));
+      password.dispatchEvent(new InputEvent('change'));
+      fixture.detectChanges();
+      expect(password.value).toEqual('abcdefghij');
+      fixture.whenStable().then(() => {
+        const password = fixture.nativeElement.querySelector('#password_error');
+        expect(password).toBeNull();
+      });
     });
   });
   describe('onSubmit', () => {
